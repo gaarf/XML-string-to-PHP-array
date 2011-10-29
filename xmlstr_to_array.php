9c899147bfec723ef2d05bc2ff6802f71de5c813
Adrien Cahen <onlineallthetime@gmail.com>
2011-10-28 20:27:48 -0700

    add a test

--- xmlstr_to_array.php
+++ xmlstr_to_array.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * convert xml string to php array - useful to get a serializable value
- *
- * @param string $xmlstr
- * @return array
- *
- * @author Adrien aka Gaarf & contributors
- * @see http://gaarf.info/2009/08/13/xml-string-to-php-array/
- */
+  * convert xml string to php array - useful to get a serializable value
+  *
+  * @param string $xmlstr
+  * @return array
+  *
+  * @author Adrien aka Gaarf & contributors
+  * @see http://gaarf.info/2009/08/13/xml-string-to-php-array/
+*/
 
 function xmlstr_to_array($xmlstr) {
   $doc = new DOMDocument();
@@ -18,43 +18,42 @@ function xmlstr_to_array($xmlstr) {
 function domnode_to_array($node) {
   $output = array();
   switch ($node->nodeType) {
-   case XML_CDATA_SECTION_NODE:
-   case XML_TEXT_NODE:
-    $output = trim($node->textContent);
-   break;
-   case XML_ELEMENT_NODE:
-    for ($i=0, $m=$node->childNodes->length; $i<$m; $i++) {
-     $child = $node->childNodes->item($i);
-     $v = domnode_to_array($child);
-     if(isset($child->tagName)) {
-       $t = $child->tagName;
-       if(!isset($output[$t])) {
-        $output[$t] = array();
-       }
-       if(empty($v)) {
-         $v = '';
-       }
-       $output[$t][] = $v;
-     }
-     elseif($v || $v === '0') {
-      $output = (string) $v;
-     }
-    }
-    if(is_array($output)) {
-     if($node->attributes->length) {
-      $a = array();
-      foreach($node->attributes as $attrName => $attrNode) {
-       $a[$attrName] = (string) $attrNode->value;
+
+    case XML_CDATA_SECTION_NODE:
+    case XML_TEXT_NODE:
+      $output = trim($node->textContent);
+    break;
+
+    case XML_ELEMENT_NODE:
+      for ($i=0, $m=$node->childNodes->length; $i<$m; $i++) {
+        $child = $node->childNodes->item($i);
+        $v = domnode_to_array($child);
+        if(isset($child->tagName)) {
+          $t = $child->tagName;
+          if(!isset($output[$t])) {
+            $output[$t] = array();
+          }
+          $output[$t][] = $v;
+        }
+        elseif($v || $v === '0') {
+          $output = (string) $v;
+        }
       }
-      $output['@attributes'] = $a;
-     }
-     foreach ($output as $t => $v) {
-      if(is_array($v) && count($v)==1 && $t!='@attributes') {
-       $output[$t] = $v[0];
+      if(is_array($output)) {
+        if($node->attributes->length) {
+          $a = array();
+          foreach($node->attributes as $attrName => $attrNode) {
+            $a[$attrName] = (string) $attrNode->value;
+          }
+          $output['@attributes'] = $a;
+        }
+        foreach ($output as $t => $v) {
+          if(is_array($v) && count($v)==1 && $t!='@attributes') {
+            $output[$t] = $v[0];
+          }
+        }
       }
-     }
-    }
-   break;
+    break;
   }
   return $output;
 }
